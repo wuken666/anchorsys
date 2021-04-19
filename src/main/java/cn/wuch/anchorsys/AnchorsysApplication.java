f216f766f1bc102fe534b41fb92cb4592fb20cb4
wuken666 <Wuken666@wuk.com>
2021-04-19 16:36:22 +0800

    项目框架

---
+++ src/main/java/cn/wuch/anchorsys/AnchorsysApplication.java
@@ -0,0 +1,13 @@
+package cn.wuch.anchorsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnchorsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnchorsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/wuch/anchorsys/AnchorsysApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wuch.anchorsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnchorsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
